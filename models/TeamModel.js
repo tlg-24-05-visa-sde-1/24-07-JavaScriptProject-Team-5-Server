d06425e3eee4777e508f4159f39dd669860e373d
Justin <JustinE@tlgcohort.com>
2024-07-31 19:23:53 -0500

    merged all Team routes and tested with Postman

--- models/TeamModel.js
+++ models/TeamModel.js
@@ -8,7 +8,7 @@ const teamSchema = new mongoose.Schema({
   rating: {
     type: Number,
   },
-  //   owner: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  owner: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
 });
 
 const Team = mongoose.model("Team", teamSchema);
