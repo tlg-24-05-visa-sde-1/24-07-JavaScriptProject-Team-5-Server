2c64f50703950760bf8ea801c327f51ec49194a5
Justin <JustinE@tlgcohort.com>
2024-08-01 15:24:17 -0500

    changed user model

--- models/User.js
+++ models/User.js
@@ -4,12 +4,21 @@ const UserSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
+  followers: {
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: "users" }],
+  },
+  following: {
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: "users" }],
+  },
+  Team: {
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: "Team" }],
+  },
 });
 
 const UserModel = mongoose.model("users", UserSchema);
 module.exports = UserModel;
 
 /*
-  UserSchema defines the structure of the documents in the usesrs collection. Users is the ame of the collection in the database
+  UserSchema defines the structure of the documents in the users collection. Users is the ame of the collection in the database
   UserModel is a Mongoose model created based on the UserSchema. It represents the users collection in MongoDB and provides methods for interacting with it.
 */
